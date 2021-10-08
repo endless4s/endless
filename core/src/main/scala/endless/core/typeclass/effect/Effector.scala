37ed96bcc864c5103c08c14598e04812ea4a474d
Jonas Chapuis <jonas.chapuis@bestmile.com>
2021-10-08 21:08:02 +0200

    Add unit helper effector
    Ready to use if no effects are needed

--- core/src/main/scala/endless/core/typeclass/effect/Effector.scala
+++ core/src/main/scala/endless/core/typeclass/effect/Effector.scala
@@ -1,5 +1,16 @@
 package endless.core.typeclass.effect
 
+import cats.{Applicative, Id}
+import endless.core.typeclass.entity.StateReader
+
 trait Effector[F[_]] {
   def afterPersist: F[Unit]
+}
+
+object Effector {
+  def unit[F[_]: Applicative, S]: StateReader[F, S] => Effector[F] = UnitEffector(_)
+  private final case class UnitEffector[F[_]: Applicative, S](reader: StateReader[F, S])
+      extends Effector[F] {
+    override def afterPersist: F[Unit] = Applicative[F].unit
+  }
 }
