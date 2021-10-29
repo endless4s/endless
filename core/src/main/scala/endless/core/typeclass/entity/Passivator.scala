4e44fa494a1ab7fde754d8da4cbbcbd8fb414327
Jonas Chapuis <jonas.chapuis@zf.com>
2021-10-29 21:15:41 +0200

    Extend Effector concept with passivation support

---
+++ core/src/main/scala/endless/core/typeclass/entity/Passivator.scala
@@ -0,0 +1,23 @@
+package endless.core.typeclass.entity
+
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+/** `Passivator` represents the ability to "passivate" an entity, i.e. flush out an entity from the
+  * system temporarily
+  * @tparam F
+  *   context
+  */
+trait Passivator[F[_]] {
+
+  /** Schedule entity passivation after the specified delay - if zero (default), passivate
+    * immediately
+    * @param after
+    *   delay before passivation
+    */
+  def enablePassivation(after: FiniteDuration = Duration.Zero): F[Unit]
+
+  /** Disable scheduled passivation (note that entities automatically recover when receiving a
+    * message)
+    */
+  def disablePassivation: F[Unit]
+}
