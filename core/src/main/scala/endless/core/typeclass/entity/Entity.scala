d525e267c5d5a515c86b108f3dcfb76226c8fcba
Jonas Chapuis <jonas.chapuis@zf.com>
2021-11-08 17:49:56 +0100

    Add EitherT convenience overloads in Entity

--- core/src/main/scala/endless/core/typeclass/entity/Entity.scala
+++ core/src/main/scala/endless/core/typeclass/entity/Entity.scala
@@ -1,6 +1,7 @@
 package endless.core.typeclass.entity
 
 import cats.Monad
+import cats.data.EitherT
 import endless.\/
 import endless.core.typeclass.event.EventWriter
 import cats.syntax.either._
@@ -31,8 +32,8 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifUnknown
     *   left value in case of missing entity
     */
-  def ifKnown[A, Error](fa: S => A)(ifUnknown: => Error): F[Error \/ A] =
-    ifKnownF[A, Error](s => pure(fa(s)))(ifUnknown)
+  def ifKnown[Error, A](fa: S => A)(ifUnknown: => Error): F[Error \/ A] =
+    ifKnownF[Error, A](s => pure(fa(s)))(ifUnknown)
 
   /** Convenience function which applies `fa` on the state if entity exists and wraps this in a
     * `Right`, otherwise returns a `Left` with the provided error value.
@@ -41,8 +42,8 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifUnknown
     *   left value in case of missing entity
     */
-  def ifKnownF[A, Error](fa: S => F[A])(ifUnknown: => Error): F[Error \/ A] =
-    ifKnownFE[A, Error](s => map(fa(s))(_.asRight))(ifUnknown)
+  def ifKnownF[Error, A](fa: S => F[A])(ifUnknown: => Error): F[Error \/ A] =
+    ifKnownFE[Error, A](s => map(fa(s))(_.asRight))(ifUnknown)
 
   /** Convenience function which applies `fa` on the state if entity exists, otherwise returns a
     * `Left` with the provided error value.
@@ -51,11 +52,21 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifUnknown
     *   left value in case of missing entity
     */
-  def ifKnownFE[A, Error](fa: S => F[Error \/ A])(ifUnknown: => Error): F[Error \/ A] =
+  def ifKnownFE[Error, A](fa: S => F[Error \/ A])(ifUnknown: => Error): F[Error \/ A] =
     flatMap(read) {
       case Some(state) => fa(state)
       case None        => pure(ifUnknown.asLeft)
     }
+
+  /** Convenience function which applies `fa` on the state if entity exists and unwraps EitherT
+    * value, otherwise returns a `Left` with the provided error value.
+    * @param fa
+    *   function to apply on state
+    * @param ifUnknown
+    *   left value in case of missing entity
+    */
+  def ifKnownT[Error, A](fa: S => EitherT[F, Error, A])(ifUnknown: => Error): F[Error \/ A] =
+    ifKnownFE(s => fa(s).value)(ifUnknown)
 
   /** Convenience function which returns a in a `Right` if entity doesn't yet exist, otherwise calls
     * `ifKnown` with the state and wraps this in a `Left`.
@@ -64,8 +75,8 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifKnown
     *   function to compute left value in case of existing entity
     */
-  def ifUnknown[A, Error](a: => A)(ifKnown: S => Error): F[Error \/ A] =
-    ifUnknownF[A, Error](pure(a))(ifKnown)
+  def ifUnknown[Error, A](a: => A)(ifKnown: S => Error): F[Error \/ A] =
+    ifUnknownF[Error, A](pure(a))(ifKnown)
 
   /** Convenience function which invokes `fa` if entity doesn't yet exist and wraps this in a
     * `Right`, otherwise calls `ifKnown` with the state and wraps this in a `Left`.
@@ -74,8 +85,8 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifKnown
     *   function to compute left value in case of existing entity
     */
-  def ifUnknownF[A, Error](fa: => F[A])(ifKnown: S => Error): F[Error \/ A] =
-    ifUnknownFE[A, Error](map(fa)(_.asRight))(ifKnown)
+  def ifUnknownF[Error, A](fa: => F[A])(ifKnown: S => Error): F[Error \/ A] =
+    ifUnknownFE[Error, A](map(fa)(_.asRight))(ifKnown)
 
   /** Convenience function which invokes `fa` if entity doesn't yet exist and wraps this in a
     * `Right`, otherwise calls `ifKnown` with the state and wraps this in a `Left`.
@@ -84,11 +95,22 @@ trait Entity[F[_], S, E] extends StateReader[F, S] with EventWriter[F, E] with M
     * @param ifKnown
     *   function to compute left value in case of existing entity
     */
-  def ifUnknownFE[A, Error](fa: => F[Error \/ A])(ifKnown: S => Error): F[Error \/ A] =
+  def ifUnknownFE[Error, A](fa: => F[Error \/ A])(ifKnown: S => Error): F[Error \/ A] =
     flatMap(read) {
       case None =>
         fa
       case Some(state) =>
         pure(ifKnown(state).asLeft)
     }
+
+  /** Convenience function which applies `fa` on the state if entity exists and unwraps EitherT
+    * value, otherwise returns a `Left` with the provided error value.
+    * @param fa
+    *   function to apply on state
+    * @param ifUnknown
+    *   left value in case of missing entity
+    */
+  def ifUnknownT[Error, A](fa: => EitherT[F, Error, A])(ifUnknown: S => Error): F[Error \/ A] =
+    ifUnknownFE(fa.value)(ifUnknown)
+
 }
