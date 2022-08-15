69c3fee2a0f870e16935980ee4e3b4f7cf330ec1
Jonas Chapuis <jonas.chapuis@zf.com>
2022-08-15 23:09:28 +0200

    Simplify and document EntityT lifting

--- core/src/main/scala/endless/core/interpret/EntityLift.scala
+++
@@ -1,8 +0,0 @@
-package endless.core.interpret
-
-import endless.core.entity.Entity
-
-/** Any effect of type G[A] can be lifted into F[A] thus enabling entity capability - we typically
-  * lift `EntityT` for interpretation
-  */
-trait EntityLift[G[_], F[_], S, E] extends Entity[G, S, E]
