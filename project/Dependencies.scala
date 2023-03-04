931dca48078a6023a436ac95de571e8c2e3e8ec7
Scala Steward <scala_steward@virtuslab.com>
2023-03-04 15:50:25 +0000

    Update circe-core, circe-generic, ... to 0.14.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
     "org.http4s" %% "http4s-blaze-client" % blazeVersion
   )
 
-  lazy val circeVersion = "0.14.3"
+  lazy val circeVersion = "0.14.5"
   lazy val circe = Seq(
     "io.circe" %% "circe-core",
     "io.circe" %% "circe-generic",
