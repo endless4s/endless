8e2799d43bf4f1ca5434b1f1be49e7b94c92fed2
Scala Steward <scala_steward@virtuslab.com>
2023-09-28 15:41:39 +0000

    Update cats-effect, cats-effect-kernel, ... to 3.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
   lazy val catsTaglessVersion = "0.15.0"
   lazy val catsTagless = Seq("org.typelevel" %% "cats-tagless-macros" % catsTaglessVersion)
 
-  lazy val catsEffectVersion = "3.5.1"
+  lazy val catsEffectVersion = "3.5.2"
   lazy val catsEffectKernel = Seq("org.typelevel" %% "cats-effect-kernel" % catsEffectVersion)
   lazy val catsEffectLaws = Seq("org.typelevel" %% "cats-effect-laws" % catsEffectVersion)
   lazy val catsEffectTestKit = Seq("org.typelevel" %% "cats-effect-testkit" % catsEffectVersion)
