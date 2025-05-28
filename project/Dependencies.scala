0a8745b10930fdd45267337ead86796d872db749
Scala Steward <scala_steward@virtuslab.com>
2025-05-28 17:47:12 +0000

    Update log4cats-core, log4cats-testing to 2.7.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
   lazy val logbackVersion = "1.5.18"
   lazy val logback = Seq("ch.qos.logback" % "logback-classic" % logbackVersion)
 
-  lazy val log4catsVersion = "2.7.0"
+  lazy val log4catsVersion = "2.7.1"
   lazy val log4cats = Seq("org.typelevel" %% "log4cats-core" % log4catsVersion)
   lazy val log4catsTesting = Seq("org.typelevel" %% "log4cats-testing" % log4catsVersion)
 
