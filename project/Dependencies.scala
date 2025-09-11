26f485e793140add74c7f6f977a74624a1d60cb6
Scala Steward <scala_steward@virtuslab.com>
2025-09-11 17:13:54 +0000

    Update munit-scalacheck to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -83,7 +83,7 @@ object Dependencies {
 
   lazy val mUnitVersion = "1.1.1"
   lazy val disciplineMUnitVersion = "2.0.0"
-  lazy val mUnitScalacheckVersion = "1.1.0"
+  lazy val mUnitScalacheckVersion = "1.2.0"
   lazy val mUnit =
     Seq(
       "org.scalameta" %% "munit" % mUnitVersion,
