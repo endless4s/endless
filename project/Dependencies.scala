88d24979c11213f6e951df3fe876ac82d02febc2
Scala Steward <scala_steward@virtuslab.com>
2026-06-26 17:15:39 +0000

    Update logback-classic to 1.5.37

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
     "io.circe" %% "circe-parser"
   ).map(_ % circeVersion)
 
-  lazy val logbackVersion = "1.5.34"
+  lazy val logbackVersion = "1.5.37"
   lazy val logback = Seq("ch.qos.logback" % "logback-classic" % logbackVersion)
 
   lazy val log4catsVersion = "2.8.0"
